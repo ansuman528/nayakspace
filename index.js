94b016ddaff3924c723f327de31f97c9b2e16a88
ansuman528 <ansumannayak528@gmail.com>
2021-04-30 00:36:09 +0530

    refresh

--- index.js
+++ index.js
@@ -1,25 +1,11 @@
-// // var typed = new Typed(".typing",{
-// //     strings:["Hi","Hola"],
-// //     typeSpeed: 100,
-// //     backSpeed:60,
-// //     loop: true,
-// //   });
 
-//   $(document).ready(function(){
-//     $(window).scroll(function(){
-//       if(this.scrollY>36){
-//         $(".navbar-holder").addClass("sticky");
-//       }
-//       else{
-//         $(".navbar-holder").removeClass("sticky");
-//       }
-//     });
-//   });
-//   $(document).ready(function(){
-//     $(".profile-photo img").mouseenter(function(){
-//       $(".profile-info").addClass("visible");
-//     });
-//     $(".profile-photo img").mouseleave(function(){
-//       $(".profile-info").removeClass("visible");
-//     });
-//   });
+  $(document).ready(function(){
+    $(window).scroll(function(){
+      if(this.scrollY>55){
+        $(".navbar").addClass("sticky");
+      }
+      else{
+        $(".navbar").removeClass("sticky");
+      }
+    });
+  });

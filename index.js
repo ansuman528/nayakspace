7a572742187acef0904abfbef553f3f5c99e3008
ansuman528 <ansumannayak528@gmail.com>
2021-04-29 10:25:03 +0530

    2nd commmit

---
+++ index.js
@@ -0,0 +1,25 @@
+// // var typed = new Typed(".typing",{
+// //     strings:["Hi","Hola"],
+// //     typeSpeed: 100,
+// //     backSpeed:60,
+// //     loop: true,
+// //   });
+
+//   $(document).ready(function(){
+//     $(window).scroll(function(){
+//       if(this.scrollY>36){
+//         $(".navbar-holder").addClass("sticky");
+//       }
+//       else{
+//         $(".navbar-holder").removeClass("sticky");
+//       }
+//     });
+//   });
+//   $(document).ready(function(){
+//     $(".profile-photo img").mouseenter(function(){
+//       $(".profile-info").addClass("visible");
+//     });
+//     $(".profile-photo img").mouseleave(function(){
+//       $(".profile-info").removeClass("visible");
+//     });
+//   });
